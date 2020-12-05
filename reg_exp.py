81430e08b67070ce40b937ed38f1a92901cafc9a
Alex Ivanov <alexivanovrus@gmail.com>
2020-12-05 14:16:49 +0300

    fixed reg_exp

--- reg_exp.py
+++ reg_exp.py
@@ -6,10 +6,11 @@ phrase_pattern = "ВТ(\W+\w+){,3}\W+ИТМО"
 word_pattern = "\w+"
 
 def proga():
+    phrase = re.search(phrase_pattern, text)
     result = re.findall(word_pattern, phrase.group(0)) 
     string = ""
     for word in result:
         string += word + " "
-    print(string) 
-    
+    print(string)    
+
 proga()    
